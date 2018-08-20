e22ae7910d967adb1fa1da29dcab8a91b7e228ad
Basavaprabhu <basavaprabhu0606@gmail.com>
2018-08-21 00:59:53 +0530

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,45 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BhajanComponent } from './bhajan/bhajan.component';
+import { BiographyComponent } from './biography/biography.component';
+import { BlogComponent } from './blog/blog.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { FooterComponent } from './footer/footer.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { PeopleInvolvedComponent } from './people-involved/people-involved.component';
+import { PublicationComponent } from './publication/publication.component';
+import { QuoteComponent } from './quote/quote.component';
+import { SaiComponent } from './sai/sai.component';
+import { ShankarComponent } from './shankar/shankar.component';
+import { StoryComponent } from './story/story.component';
+import { TeachingsComponent } from './teachings/teachings.component';
+import { AppRoutingModule } from './/app-routing.module';
+import { NgwWowModule } from 'ngx-wow';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BhajanComponent,
+    BiographyComponent,
+    BlogComponent,
+    ContactUsComponent,
+    FooterComponent,
+    GalleryComponent,
+    NavbarComponent,
+    PeopleInvolvedComponent,
+    PublicationComponent,
+    QuoteComponent,
+    SaiComponent,
+    ShankarComponent,
+    StoryComponent,
+    TeachingsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    NgwWowModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/bhajan/bhajan.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bhajan',
+  templateUrl: './bhajan.component.html',
+  styleUrls: ['./bhajan.component.css']
+})
+export class BhajanComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
